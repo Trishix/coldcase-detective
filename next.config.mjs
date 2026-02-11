15de797f28384d53b2826b892b9589b76948a1d2
Trishix <trishitofficial@gmail.com>
2026-02-12 02:45:37 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
         config.externals.push('onnxruntime-node', 'sharp');
         return config;
     },
+    experimental: {
+        outputFileTracingExcludes: {
+            '*': [
+                './node_modules/@lancedb/lancedb-linux-x64-musl/**/*', // Alpine Linux
+                './node_modules/@lancedb/lancedb-darwin-x64/**/*', // Intel Mac
+                './node_modules/@lancedb/lancedb-darwin-arm64/**/*', // Apple Silicon
+                './node_modules/@lancedb/lancedb-win32-x64/**/*', // Windows
+            ],
+        },
+    },
 };
 
 export default nextConfig;
